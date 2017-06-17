cbc752ccfafae4d32ba0676cfd12343dea92a04e
Paul Wenzel <wenzel.pgb@googlemail.com>
2017-06-18 00:25:12 +0200

    Changed Models to abstract classes

--- src/providers/model/entry-model.ts
+++ src/providers/model/entry-model.ts
@@ -3,18 +3,13 @@ import { Department } from "./department-model";
 /**
  * Entry class for usage in model and controller. Class defines how entries should be specified
  */
-export class Entry {
-    ////////////////////////////////////////////Properties/////////////////////////////////////////////
-    private name: String;
-    private id: number;
+export abstract class Entry {
+    abstract get _id():number;
+
+    abstract get name():string;
+    abstract set name(name: string);
 
     // the index of the department matches the departmendId for easier access and less effort to find the correct department
-    private departments: Array<Department>
-
-    ////////////////////////////////////////////Constructor////////////////////////////////////////////
-    constructor(name: String, id: number, departments: Array<Department>) {
-        this.name = name;
-        this.id = id;
-        this.departments = departments
-    }
+    abstract get departments():Array<Department>;
+    abstract set departments(departments: Array<Department>);
 }

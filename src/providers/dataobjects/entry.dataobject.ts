023263037e85eb762c91c36728aad2d0b88dc808
simonweber <simonweber96@gmail.com>
2017-06-22 11:28:56 +0200

    Adjust dataobjects

--- src/providers/dataobjects/entry.dataobject.ts
+++ src/providers/dataobjects/entry.dataobject.ts
@@ -1,4 +1,5 @@
 import {DepartmentEntryDescriptionDataobject} from "./department-entry-description.dataobject";
+import { AttachmentDataobject } from "./attachment.dataobject";
 
 /**
  * EntryDataobject class for usage in model and controller. Class defines how entries should be specified
@@ -9,5 +10,12 @@ export abstract class EntryDataobject {
   abstract get name(): string;
   abstract set name(name: string);
 
-  abstract get departments(): Array<DepartmentEntryDescriptionDataobject>;
+  abstract get description(): string;
+  abstract set description(description: string);
+
+  abstract get relatedDepartments(): Array<number>;
+
+  abstract get attachments(): Array<AttachmentDataobject>;
+
+  abstract get departmentSpecifics(): Array<DepartmentEntryDescriptionDataobject>;
 }

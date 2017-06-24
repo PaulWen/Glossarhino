93fda6f48fa195d358b809f8796d41b0c84fc19b
simonweber <simonweber96@gmail.com>
2017-06-24 13:26:04 +0200

    Added AttachmentModalPage

---
+++ src/pages/attachment-modal/attachment-modal.module.ts
@@ -0,0 +1,17 @@
+import {NgModule} from "@angular/core";
+import {IonicPageModule} from "ionic-angular";
+import {AttachmentModalPage} from "./attachment-modal";
+
+@NgModule({
+  declarations: [
+    AttachmentModalPage
+  ],
+  imports: [
+    IonicPageModule.forChild(AttachmentModalPage)
+  ],
+  exports: [
+    AttachmentModalPage
+  ]
+})
+export class AttachmentModalPageModule {
+}
---
+++ src/pages/attachment-modal/attachment-modal.ts
@@ -0,0 +1,48 @@
+import {Component} from "@angular/core";
+import {IonicPage, NavController, NavParams, ViewController} from "ionic-angular";
+import { AttachmentDataObject } from "../../providers/dataobjects/attachment.dataobject";
+
+@IonicPage()
+@Component({
+  selector: "page-attachment-modal",
+  templateUrl: "attachment-modal.html"
+})
+export class AttachmentModalPage {
+  ////////////////////////////////////////////Properties/////////////////////////////////////////////
+  // ionic injected components
+  private navCtrl: NavController;
+  private navParams: NavParams;
+  private viewCtrl: ViewController;
+
+  // navParam
+  private attachments: Array<AttachmentDataObject>;
+
+  ////////////////////////////////////////////Constructor////////////////////////////////////////////
+  constructor(navCtrl: NavController, navParams: NavParams, viewCtrl: ViewController) {
+    // instantiate ionic injected components
+    this.navCtrl = navCtrl;
+    this.navParams = navParams;
+    this.viewCtrl = viewCtrl;
+
+    //get navParams
+    this.attachments = this.navParams.get("attachments");
+  }
+
+  /////////////////////////////////////////////Methods///////////////////////////////////////////////
+  /**
+   * Method to close the AttachmentModal when pressing the assigned button
+   */
+  private closeAttachmentModal() {
+    this.viewCtrl.dismiss();
+  }
+
+  /**
+   * Method to open an attachment when selected by the user. Will open in the systems browser.
+   * @param url
+   */
+  private openAttachment(url: string) {
+    //window.location.href = url.href;
+    window.open(url, "_system");
+  }
+
+}

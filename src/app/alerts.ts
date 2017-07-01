76643b0db8b8a30b3914c6b203744be195736ad7
Paul Wenzel <wenzel.pgb@googlemail.com>
2017-07-01 19:23:45 +0200

    Fixed bug in DepartmentFilterAlert

--- src/app/alerts.ts
+++ src/app/alerts.ts
@@ -1,97 +1,87 @@
-import { AppModelService } from "../providers/app-model-service";
-import { GlobalDepartmentConfigDataObject } from "../providers/dataobjects/global-department-config.dataobject";
-import { UserDepartmentFilterConfigDataObject } from "../providers/dataobjects/user-department-filter-config.dataobject";
-import { DepartmentDataObject } from "../providers/dataobjects/department.dataobject";
-import { AlertController } from "ionic-angular";
-import { Logger } from "./logger";
+import {AlertController} from "ionic-angular";
+import {AppModelService} from "../providers/app-model-service";
+import {DepartmentDataObject} from "../providers/dataobjects/department.dataobject";
+import {GlobalDepartmentConfigDataObject} from "../providers/dataobjects/global-department-config.dataobject";
+import {UserDepartmentFilterConfigDataObject} from "../providers/dataobjects/user-department-filter-config.dataobject";
+import {Logger} from "./logger";
 
 /**
  * This class implements all the alerts needed for the app
  */
 export class Alerts {
 
-    ////////////////////////////////////////////Properties////////////////////////////////////////////
+  ////////////////////////////////////////////Properties////////////////////////////////////////////
 
-    /////////////////////////////////////////////Methods///////////////////////////////////////////////
+  /////////////////////////////////////////////Methods///////////////////////////////////////////////
 
-    //////////////////////////////////////////
-    //       Department Filter Alert        //
-    //////////////////////////////////////////
+  //////////////////////////////////////////
+  //       Department Filter Alert        //
+  //////////////////////////////////////////
 
-    private static async loadMergedDepartmentConfig(appModelService: AppModelService): Promise<Array<{ details: DepartmentDataObject, checked: boolean }>> {
-        let globalDepartmentConfig: GlobalDepartmentConfigDataObject = appModelService.getGlobalDepartmentConfigDataObject();
+  private static mergeGlobalDepartmentConfigWithUserPreferences(globalDepartmentConfig: GlobalDepartmentConfigDataObject, userDepartmentFilterConfig: UserDepartmentFilterConfigDataObject): Array<{ details: DepartmentDataObject, checked: boolean }> {
+    let mergedDepartmentConfig: Array<{ details: DepartmentDataObject, checked: boolean }> = [];
 
-        //load user department filter preferences
-        let userDepartmentFilterConfig: UserDepartmentFilterConfigDataObject = await appModelService.getUserDepartmentFilterConfigDataObject()
+    globalDepartmentConfig.departments.forEach(department => {
+      if (userDepartmentFilterConfig.selectedDepartments.find(selectedDepartment => selectedDepartment == department.departmentId) == undefined) {
+        mergedDepartmentConfig.push({
+          details: department,
+          checked: false
+        });
+      } else {
+        mergedDepartmentConfig.push({
+          details: department,
+          checked: true
+        });
+      }
+    });
+    return mergedDepartmentConfig;
+  }
 
-        // merge global with user config
+  public static showDepartmentFilterAlert(alertCtrl: AlertController, appModelService: AppModelService): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+      // load data to create inputs for all departments
+      let globalDepartmentConfig: GlobalDepartmentConfigDataObject = appModelService.getGlobalDepartmentConfigDataObject();
+      appModelService.getUserDepartmentFilterConfigDataObject().then((userDepartmentFilterConfig: UserDepartmentFilterConfigDataObject) => {
         let mergedDepartmentConfig: Array<{ details: DepartmentDataObject, checked: boolean }> = this.mergeGlobalDepartmentConfigWithUserPreferences(globalDepartmentConfig, userDepartmentFilterConfig);
 
-        return mergedDepartmentConfig;
+        // create dialog
+        let departmentFilterAlert = alertCtrl.create();
+        departmentFilterAlert.setTitle("Select departments");
 
-    }
 
-    private static mergeGlobalDepartmentConfigWithUserPreferences(globalDepartmentConfig: GlobalDepartmentConfigDataObject, userDepartmentFilterConfig: UserDepartmentFilterConfigDataObject): Array<{ details: DepartmentDataObject, checked: boolean }> {
-        let mergedDepartmentConfig: Array<{ details: DepartmentDataObject, checked: boolean }> = [];
-
-        globalDepartmentConfig.departments.forEach(department => {
-            if (userDepartmentFilterConfig.selectedDepartments.find(selectedDepartment => selectedDepartment == department.departmentId) == undefined) {
-                mergedDepartmentConfig.push({
-                    details: department,
-                    checked: false
-                });
-            } else {
-                mergedDepartmentConfig.push({
-                    details: department,
-                    checked: true
-                });
-            }
+        // create inputs for the departments
+        mergedDepartmentConfig.forEach(department => {
+          departmentFilterAlert.addInput({
+            type: "checkbox",
+            label: department.details.departmentName,
+            value: department.details.departmentId.toString(),
+            checked: department.checked
+          });
         });
-        return mergedDepartmentConfig;
-    }
 
-    public static showDepartmentFilterAlert(alertCtrl: AlertController, appModelService: AppModelService): Promise<boolean> {
-        return new Promise((resolve, reject) => {
+        // add cancel button
+        departmentFilterAlert.addButton("Cancel");
 
-            let departmentFilterAlert = alertCtrl.create();
-            departmentFilterAlert.setTitle("Select departments");
-
-            // load data to create inputs for all departments
-            this.loadMergedDepartmentConfig(appModelService).then((data) => {
-                let mergedDepartmentConfig = data;
-
-                // create inputs for the departments
-                mergedDepartmentConfig.forEach(department => {
-                    departmentFilterAlert.addInput({
-                        type: "checkbox",
-                        label: department.details.departmentName,
-                        value: department.details.departmentId.toString(),
-                        checked: department.checked
-                    });
-                });
-
-                // add cancel button
-                departmentFilterAlert.addButton("Cancel");
-
-                // add okay button
-                departmentFilterAlert.addButton({
-                    text: "OK",
-                    handler: data => {
-                        resolve(appModelService.setUserDepartmentFilterConfigDataObject(data));
-                    }
-                });
-
-                // show alert
-                departmentFilterAlert.present();
-
-            }, (error) => {
-                Logger.log("Loading mergedDepartmentConfig failed (Class: Alerts, Method: showDepartmentFilterAlert()");
-                Logger.error(error);
-                reject(error);
+        // add okay button
+        departmentFilterAlert.addButton({
+          text: "OK",
+          handler: data => {
+            // update user settings
+            userDepartmentFilterConfig.selectedDepartments = data;
+            appModelService.setUserDepartmentFilterConfigDataObject(userDepartmentFilterConfig).then((data) => {
+              resolve(data);
             });
-
+          }
         });
 
-    }
+        // show alert
+        departmentFilterAlert.present();
 
+      }, (error) => {
+        Logger.log("Loading mergedDepartmentConfig failed (Class: Alerts, Method: showDepartmentFilterAlert()");
+        Logger.error(error);
+        reject(error);
+      });
+    });
+  }
 }
